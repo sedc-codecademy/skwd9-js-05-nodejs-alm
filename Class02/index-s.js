c2c771a724d992c3b358db1854974ea7573a29fc
ivokostovski <ivo.kostovski@gmail.com>
2021-04-03 12:07:07 +0200

    edit implemented

--- Class02/index-s.js
+++ Class02/index-s.js
@@ -1,10 +1,25 @@
 const studentService = require('./students-service');
 
-let student = {
+let peter = {
+    id: 1, 
     name: "Peter",
     age: 20,
     city: "Skopje",
     academy: 'Code'
 }
 
-studentService.addStudent(student);
+let josh = {
+    id: 2,
+    name: "Josh",
+    age: 30,
+    city: "Skopje",
+    academy: 'Code'
+}
+
+// studentService.addStudent(peter);
+
+// studentService.deleteStudent(1);
+
+studentService.editStudent(2, { age: 31 })
+
+console.log(studentService.getStudents())
--- Class02/students-service.js
+++ Class02/students-service.js
@@ -2,27 +2,47 @@ const fs = require('fs');
 
 const getStudents = () => {
     let data = fs.readFileSync('./students.json');
-
     let parsedData = JSON.parse(data);
-
     return parsedData.students;
 }
 
 const addStudent = (student) => {
     let students = getStudents();
-
-    students = [...students, student]
-
-    let data = {
-        students
+    const exists = students.find(s => s.id === student.id)
+    if (exists) {
+        throw new Error(`The student ${student.name} with ID: ${student.id} already exists in the DB.`)
     }
+    students = [...students, student]
+    saveStudents(students);
+}
 
+const editStudent = (id, editedStudent) => {
+    let students = getStudents();
+    let index = students.findIndex(s => s.id === id);
+    if (!index && index !== 0) {
+        throw new Error('No such student in db!')
+    }
+    let student = students[index];
+    student = { ...student, ...editedStudent }
+    students[index] = student;
+    saveStudents(students);
+}
+
+const deleteStudent = (id) => {
+    let students = getStudents();
+    students = students.filter(s => s.id !== id);
+    saveStudents(students);
+}
+
+const saveStudents = (students) => {
+    let data = { students }
     let stringifiedData = JSON.stringify(data);
-
     fs.writeFileSync('./students.json', stringifiedData)
 }
 
 module.exports = {
     getStudents,
-    addStudent
+    addStudent,
+    editStudent,
+    deleteStudent
 }
