ea6e1223608e590df1fda33256dbd41aab547c13
Ivan Lazarevski <Ivan.Lazarevski@seavus.com>
2021-04-05 16:50:23 +0200

    starter code for class 3 added

---
+++ Class03/app.js
@@ -0,0 +1,8 @@
+const getReviewsBtn = document.querySelector("#get-reviews");
+const postReviewsBtn = document.querySelector("#post-review");
+
+const titleInput = document.querySelector("#film-title");
+const scoreInput = document.querySelector("#film-score");
+const textInput = document.querySelector("#film-text");
+
+const reviewsListSection = document.querySelector("#posts-list");
