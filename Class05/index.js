5d4083811ce4b74073f077629974c4c61d3ff099
ivokostovski <ivo.kostovski@gmail.com>
2021-04-12 19:37:32 +0200

    Update index.js

--- Class05/index.js
+++ Class05/index.js
@@ -1,1 +1,30 @@
-console.log('testing 2')
+// const http = require('http');
+
+// importing express
+const express = require('express');
+
+// creating application
+const app = express();
+
+app.use((req, res, next) => {
+    console.log('This is a middleware')
+    next();
+})
+
+app.use('/add-animal', (req, res, next) => {
+    res.send(`<form action="" method="">
+                <input type="text" name="" />
+                <button type="submit">Submit</button> 
+            </form>`)
+})
+
+app.use('/', (req, res, next) => {
+    res.send('<h1>Hello Word</h1>')
+})
+
+
+
+app.listen(3000);
+
+// const server = http.createServer(app)
+// server.listen(3000)
