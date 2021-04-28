fb5ad4930488593feaacd1b6c7049583f8a880bc
Ivan Lazarevski <Ivan.Lazarevski@seavus.com>
2021-04-28 21:04:37 +0200

    code completed

--- Class11_serverside-render/routes/users.routes.js
+++ Class11_serverside-render/routes/users.routes.js
@@ -8,7 +8,12 @@ router.get('/', (req, res) => {
     const data = JSON.parse(text);
 
     // res.status(200).json(data);
-    res.sendFile(path.join(root ,'views', 'users-list.html'));
+    // res.sendFile(path.join(root ,'views', 'users-list.html'));
+    res.status(200).render('users-list', {
+        users: data,
+        pageTitle: 'Users',
+        hasUsers: data.length > 0
+    })
 });
 
 module.exports = router;
--- Class11_serverside-render/server.js
+++ Class11_serverside-render/server.js
@@ -4,6 +4,9 @@ const path = require('path');
 
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
 const PORT = process.env.PORT || 3000;
 const HOST = process.env.HOST || '0.0.0.0';
 
@@ -13,6 +16,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use('/api', router);
 
+// [404 Page]
 app.use((req, res) => {
     // res.status(404).json({
     //     message: 'Page not found!',
@@ -21,7 +25,13 @@ app.use((req, res) => {
     // path.join('hello', 'from', 'SEDC');
     // 'hello/from/sedc'
 
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    // res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    const img = 
+    'https://images.unsplash.com/photo-1598398386929-4d5370672e9f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=626&q=80'
+    res.status(404).render('404', {
+        pageTitle: 'Page Not Found',
+        imgSrc: img
+    })
 });
 
 app.listen(PORT, HOST, () => {
